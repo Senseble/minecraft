83af9163085beca3f99cebb46c4fc993101828a8
Senseble <senseblegaming@gmail.com>
2013-12-14 20:07:22 -0800

    Init

--- main/items/ItemInfo.java
+++ main/items/ItemInfo.java
@@ -1,5 +1,9 @@
 package main.items;
 
 public class ItemInfo {
-
+	public static final String IT1_UNLOCAL = "It1 link";
+	public static final String IT1_NAME = "Tier 1 link";
+	public static final String IT1_KEY = "Tier1Link";
+	public static final int IT1_DEFAULT = 4504;
+	public static int IT1_ID;
 }
